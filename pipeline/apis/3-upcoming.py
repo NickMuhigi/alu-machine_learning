b6d81500a2040102d1ec0c364685a91baa3d4e99
NickMuhigi <144104544+NickMuhigi@users.noreply.github.com>
2025-06-29 23:20:29 +0200

    Completed

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -2,39 +2,35 @@
 """
 Uses the (unofficial) SpaceX API to print the upcoming launch as:
 <launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
-
-The “upcoming launch” is the one which is the soonest from now, in UTC
-and if 2 launches have the same date, it's the first one in the API result.
 """
-
 
 import requests
 
-
 if __name__ == "__main__":
-    url = "https://api.spacexdata.com/v4/launches/upcoming"
-    results = requests.get(url).json()
-    dateCheck = float('inf')
-    launchName = None
-    rocket = None
-    launchPad = None
-    location = None
-    for launch in results:
-        launchDate = launch.get('date_unix')
-        if launchDate < dateCheck:
-            dateCheck = launchDate
-            date = launch.get('date_local')
-            launchName = launch.get('name')
-            rocket = launch.get('rocket')
-            launchPad = launch.get('launchpad')
-    if rocket:
-        rocket = requests.get('https://api.spacexdata.com/v4/rockets/{}'.
-                              format(rocket)).json().get('name')
-    if launchPad:
-        launchpad = requests.get('https://api.spacexdata.com/v4/launchpads/{}'.
-                                 format(launchPad)).json()
-        launchPad = launchpad.get('name')
-        location = launchpad.get('locality')
+    launches_url = "https://api.spacexdata.com/v4/launches/upcoming"
+    launches = requests.get(launches_url).json()
 
-    print("{} ({}) {} - {} ({})".format(
-        launchName, date, rocket, launchPad, location))
+    # Get launch with earliest date_unix
+    earliest = None
+    for launch in launches:
+        if earliest is None or launch["date_unix"] < earliest["date_unix"]:
+            earliest = launch
+
+    # Get relevant data
+    launch_name = earliest["name"]
+    date_local = earliest["date_local"]
+    rocket_id = earliest["rocket"]
+    pad_id = earliest["launchpad"]
+
+    # Get rocket name
+    rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
+    rocket_name = requests.get(rocket_url).json().get("name")
+
+    # Get launchpad name and locality
+    pad_url = f"https://api.spacexdata.com/v4/launchpads/{pad_id}"
+    pad_info = requests.get(pad_url).json()
+    pad_name = pad_info.get("name")
+    pad_locality = pad_info.get("locality")
+
+    # Final output
+    print(f"{launch_name} ({date_local}) {rocket_name} - {pad_name} ({pad_locality})")
